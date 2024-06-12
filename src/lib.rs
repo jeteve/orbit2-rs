852464164f2e63dd0d38a85f900e76ab6d33d7ed
Jerome Eteve <jerome.eteve@gmail.com>
2024-06-12 22:55:58 +0000

    Basic test

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,22 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use core::ptr;
+    use std::{borrow::BorrowMut, ffi::CString};
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn base_client() {
+        let cfilename = CString::new("foo.txt").unwrap();
+        let filename: *const super::CORBA_char = cfilename.as_ptr();
+        let mut ev: super::CORBA_Environment = unsafe { std::mem::zeroed() };
+        unsafe { super::CORBA_exception_init(ptr::addr_of_mut!(ev)) };
+
+        let argc: std::ffi::c_int = 1;
+        let mut carg1 = CString::new("bla").unwrap();
+        let arg1 = carg1.into_raw();
+        // TODO: let orb = super::CORBA_ORB_init(ptr::addr_of_mut!(argc));
     }
 }
